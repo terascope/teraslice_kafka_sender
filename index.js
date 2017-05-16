ebe0f119ec95b28c6d2505ff61f6e710e355087a
Kimbro Staken <kstaken@terascope.io>
2017-05-15 17:25:00 -0700

    First pass at a kafka sender. Main issue is that the connection information needs a terafoundation connector

---
+++ index.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var Promise = require("bluebird");
+
+function newProcessor(context, opConfig) {
+    var producer_ready = false;
+
+    // TODO: all the connection information will need to move into
+    // a terafoundation connector
+    var Kafka = require("node-rdkafka");
+
+    var producer = new Kafka.Producer({
+      'metadata.broker.list': 'localhost:9092',
+      'queue.buffering.max.messages': 500000,
+      'queue.buffering.max.ms': 1000,
+      'batch.num.messages': 100000,
+    });
+
+    producer.connect();
+
+    producer.on('ready', function() {
+        producer_ready = true;
+    });
+
+    return function(data) {
+        return new Promise(function(resolve, reject) {
+            function error(err) {
+                reject(err);
+            }
+
+            function process() {
+                if (producer_ready) {
+                    data.forEach(function(record) {
+                        var key = null;
+                        var timestamp = null;
+
+                        if (opConfig.id_field) {
+                            // TODO: make sure the field really exists
+                            key = record[opConfig.id_field];
+                        }
+
+                        if (opConfig.timestamp_field) {
+                            // TODO: make sure the field really contains a date
+                            timestamp = new Date(record[opConfig.timestamp_field]).getTime();
+                        }
+                        else if (opConfig.timestamp_now) {
+                            timestamp = Date.now();
+                        }
+
+                        producer.produce(
+                            opConfig.topic,
+                            null, // This is the partition. There may be use cases where we'll need to control this.
+                            new Buffer(JSON.stringify(record)),
+                            key,
+                            timestamp
+                        );
+                    });
+
+                    // TODO: this flush timeout may need to be configurable
+                    producer.flush(30000, function(err) {
+                        // Remove the error listener so they don't accrue across slices.
+                        producer.removeListener('event.error', error);
+
+                        if (err) {
+                            return reject(err);
+                        }
+
+                        resolve(data)
+                    });
+                }
+                else {
+                    setTimeout(process, 100);
+                }
+            }
+
+            producer.on('event.error', error);
+
+            process();
+        });
+    }
+}
+
+function schema(){
+    return {
+        topic: {
+            doc: 'Name of the Kafka topic to send data to',
+            default: '',
+            format: 'required_String'
+        },
+        id_field: {
+            doc: 'Field in the incoming record that contains keys',
+            default: '',
+            format: String
+        },
+        timestamp_field: {
+            doc: 'Field in the incoming record that contains a timestamp to set on the record',
+            default: '',
+            format: String
+        },
+        timestamp_now: {
+            doc: 'Set to true to have a timestamp generated as records are added to the topic',
+            default: '',
+            format: String
+        }
+    }
+}
+
+module.exports = {
+    newProcessor: newProcessor,
+    schema: schema
+};
