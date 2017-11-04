72eb238be9c8a786e510b1dd4b3a5a6aeb3064a7
Kimbro Staken <kstaken@terascope.io>
2017-11-03 17:13:37 -0700

    Adding eslintrc and updating code to match style

--- index.js
+++ index.js
@@ -1,84 +1,83 @@
 'use strict';
 
-var Promise = require("bluebird");
+const Promise = require('bluebird');
 
 function newProcessor(context, opConfig) {
-    var producer_ready = false;
+    let producerReady = false;
 
-    var producer = context.foundation.getConnection({
-        type: "kafka",
+    const producer = context.foundation.getConnection({
+        type: 'kafka',
         endpoint: opConfig.connection,
         options: {
-            type: "producer"
+            type: 'producer'
         },
         rdkafka_options: {
             'compression.codec': opConfig.compression
         }
     }).client;
 
-    producer.on('ready', function() {
-        producer_ready = true;
+    producer.on('ready', () => {
+        producerReady = true;
     });
 
-    return function(data) {
-        return new Promise(function(resolve, reject) {
-            function error(err) {
-                reject(err);
+    return data => new Promise(((resolve, reject) => {
+        function error(err) {
+            reject(err);
+        }
+
+        function process() {
+            if (producerReady) {
+                data.forEach((record) => {
+                    let key = null;
+                    let timestamp = null;
+
+                    if (opConfig.id_field) {
+                        // TODO: make sure the field really exists
+                        key = record[opConfig.id_field];
+                    }
+
+                    if (opConfig.timestamp_field) {
+                        // TODO: make sure the field really contains a date
+                        timestamp = new Date(record[opConfig.timestamp_field]).getTime();
+                    } else if (opConfig.timestamp_now) {
+                        timestamp = Date.now();
+                    }
+
+                    producer.produce(
+                        opConfig.topic,
+                        // This is the partition. There may be use cases where
+                        // we'll need to control this.
+                        null,
+                        new Buffer(JSON.stringify(record)),
+                        key,
+                        timestamp
+                    );
+                });
+
+                // TODO: this flush timeout may need to be configurable
+                producer.flush(30000, (err) => {
+                    // Remove the error listener so they don't accrue across slices.
+                    producer.removeListener('event.error', error);
+
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(data);
+                });
+            } else {
+                setTimeout(process, 100);
             }
+        }
 
-            function process() {
-                if (producer_ready) {
-                    data.forEach(function(record) {
-                        var key = null;
-                        var timestamp = null;
+        producer.on('event.error', error);
 
-                        if (opConfig.id_field) {
-                            // TODO: make sure the field really exists
-                            key = record[opConfig.id_field];
-                        }
-
-                        if (opConfig.timestamp_field) {
-                            // TODO: make sure the field really contains a date
-                            timestamp = new Date(record[opConfig.timestamp_field]).getTime();
-                        }
-                        else if (opConfig.timestamp_now) {
-                            timestamp = Date.now();
-                        }
-
-                        producer.produce(
-                            opConfig.topic,
-                            null, // This is the partition. There may be use cases where we'll need to control this.
-                            new Buffer(JSON.stringify(record)),
-                            key,
-                            timestamp
-                        );
-                    });
-
-                    // TODO: this flush timeout may need to be configurable
-                    producer.flush(30000, function(err) {
-                        // Remove the error listener so they don't accrue across slices.
-                        producer.removeListener('event.error', error);
-
-                        if (err) {
-                            return reject(err);
-                        }
-
-                        resolve(data);
-                    });
-                }
-                else {
-                    setTimeout(process, 100);
-                }
-            }
-
-            producer.on('event.error', error);
-
-            process();
-        });
-    }
+        process();
+    }));
 }
 
-function schema(){
+
+function schema() {
     return {
         topic: {
             doc: 'Name of the Kafka topic to send data to',
@@ -108,12 +107,12 @@ function schema(){
         compression: {
             doc: 'Type of compression to use',
             default: 'gzip',
-            format: ['none','gzip','snappy','lz4']
+            format: ['none', 'gzip', 'snappy', 'lz4']
         }
-    }
+    };
 }
 
 module.exports = {
-    newProcessor: newProcessor,
-    schema: schema
+    newProcessor,
+    schema
 };
